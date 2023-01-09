41251d66b3f5baf23954da5fd671aa4db5167382
Cyro292 <78793822+Cyro292@users.noreply.github.com>
2023-01-09 21:05:03 +0100

    Update NewPasswordView.vue

--- creo_ink/frontend/src/views/NewPasswordView.vue
+++ creo_ink/frontend/src/views/NewPasswordView.vue
@@ -1,28 +1,24 @@
 <script>
-	// @ is an alias to /src
-    import Notepad from "@/components/Notepad.vue";
+// @ is an alias to /src
+import Notepad from "@/components/Notepad.vue";
 
-	export default {
-		name: "NewPasswordn",
-		components: {
-            Notepad,
-		},
-	};
+export default {
+	name: "NewPasswordn",
+	components: {
+		Notepad,
+	},
+};
 </script>
 
-
 <template>
-
 	<body>
+		<h1><span>Create new</span> password</h1>
 
-        <h1><span>Create new</span> password</h1>
-
-        <Notepad
-        note = "Use 8 or more characters with a mix of letters, numbers & symbols."
-        confirmpassword = 1
-        newpassword = 1
-		blue = "Log in"
-        />
-    </body>
-
+		<Notepad
+			note="Use 8 or more characters with a mix of letters, numbers & symbols."
+			confirmpassword="1"
+			newpassword="1"
+			blue="Log in"
+		/>
+	</body>
 </template>
