dc36872e63c67eeecddd5397b34aaa250251888e
Flel57 <florhelfrich@gmail.com>
2023-05-01 12:20:48 +0200

    bug fixes and center alignment of canvas1

--- creo_ink/frontend/src/components/canvas/canvas.js
+++ creo_ink/frontend/src/components/canvas/canvas.js
@@ -1,23 +1,28 @@
 // Definitions
 
-
-//permanent canvas that holds all drawings
-const canvas1 = document.querySelector('#canvas1'); 
-const ctx1 = canvas1.getContext('2d');
-const rc1 = rough.canvas(document.querySelector('#canvas1'));
-
-canvas1.width = window.innerWidth * 3;
-canvas1.height = window.innerHeight * 3;
-
-
+var windowW = window.innerWidth;    //!!!  problematic since window-alignment only takes place when refreshing
+var windowH = window.innerHeight;   //!!!!  in case you scale canvas down or up canvas2 doesen't scale accordingly and drawable section on the screen decreases
 
 //temporary canvas that gets engaged by user
 const canvas2 = document.querySelector('#canvas2');
 const ctx2 = canvas2.getContext('2d');
 const rc2 = rough.canvas(document.querySelector('#canvas2'));
 
-canvas2.width = window.innerWidth;
-canvas2.height = window.innerWidth;
+canvas2.width = windowW;
+canvas2.height = windowH;
+
+//permanent canvas that holds all drawings
+const canvas1 = document.querySelector('#canvas1'); 
+const ctx1 = canvas1.getContext('2d');
+const rc1 = rough.canvas(document.querySelector('#canvas1'));
+
+canvas1.width = windowW * 3;
+canvas1.height = windowH * 3;
+
+//align canvas1 in center
+canvas1.style.left = '-1920px';
+canvas1.style.top = '-502px';
+
 
 
 
@@ -391,7 +396,7 @@ function mouseClick(e) {
 }
 
 
-//Canvas_Draw-functions
+///Canvas_Draw-functions
 
 function dragCanvas(e) {
     
